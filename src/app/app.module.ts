ed330e27380e3b473cdb380386c3eab2ab1ca13c
yader-code <jhadermoreno@hotmail.com>
2018-03-01 12:16:03 -0500

    creacion de componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,23 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { HomepageComponent } from './components/homepage/homepage.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { RegisterPageComponent } from './components/register-page/register-page.component';
+import { LoginPageComponent } from './components/login-page/login-page.component';
+import { PrivatePageComponent } from './components/private-page/private-page.component';
+import { NotFoundPageComponent } from './components/not-found-page/not-found-page.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomepageComponent,
+    NavbarComponent,
+    RegisterPageComponent,
+    LoginPageComponent,
+    PrivatePageComponent,
+    NotFoundPageComponent
   ],
   imports: [
     BrowserModule,
