d4bd7da23d9d29c5ec0d214d6d320fe2b7bfa29e
yader-code <jhadermoreno@hotmail.com>
2018-03-01 19:19:27 -0500

    routing agregado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,12 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
-import { HomepageComponent } from './components/homepage/homepage.component';
-import { NavbarComponent } from './components/navbar/navbar.component';
-import { RegisterPageComponent } from './components/register-page/register-page.component';
-import { LoginPageComponent } from './components/login-page/login-page.component';
-import { PrivatePageComponent } from './components/private-page/private-page.component';
-import { NotFoundPageComponent } from './components/not-found-page/not-found-page.component';
+import { HomepageComponent } from './componentes/homepage/homepage.component';
+import { NavbarComponent } from './componentes/navbar/navbar.component';
+import { RegisterPageComponent } from './componentes/register-page/register-page.component';
+import { LoginPageComponent } from './componentes/login-page/login-page.component';
+import { PrivatePageComponent } from './componentes/private-page/private-page.component';
+import { NotFoundPageComponent } from './componentes/not-found-page/not-found-page.component';
 
 
 @NgModule({
